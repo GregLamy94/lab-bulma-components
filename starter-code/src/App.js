b8001a302e1166f14985dc5ce1ffcedb88ed702a
Gregoire Lamy <g.aderlamy@gmail.com>
2020-01-28 21:31:46 +0100

    ?il se passe qqchose mais je sais pas quoi

---
+++ starter-code/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./Navbar";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="Navbar">
+        <Navbar/>
+      </div>
+    );
+  }
+}
